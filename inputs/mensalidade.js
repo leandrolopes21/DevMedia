e4ecbeebec01f7b1ed8fc00e29f5e9b93354b18f
Leandro Lopes <leandrolopeslive21@hotmail.com>
2025-10-25 20:53:49 -0300

    Atualizado mensalidade.js

--- inputs/mensalidade.js
+++ inputs/mensalidade.js
@@ -1,8 +1,25 @@
-let valor_mensalidade = 100;
+import mensal from 'readline-sync';
+
+// let valor_mensalidade = 100;
 let taxa_multa = 0.02; // 2%
 let total_dias = 10;
 let contador = 1;
 
+let valor_mensalidade = mensal.question("Qual o valor da mensalidade? ");
+
+while (contador < total_dias) {
+
+    let dias_atraso = contador;
+    let valor_multa = valor_mensalidade * taxa_multa * dias_atraso;
+    let novo_valor_mensalidade = valor_mensalidade + valor_multa;
+
+    console.log("Dia(s) de atraso: " + dias_atraso + " - mensalidade atualizada: R$" + novo_valor_mensalidade + ",00");
+
+    contador++;
+    
+}
+
+/*
 do {
 
     let dias_atraso = contador;
@@ -13,3 +30,4 @@ do {
 
     contador++;
 } while (contador < total_dias);
+ */
