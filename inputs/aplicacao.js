a2c32ffd6cd9e52344bae44a06a779387205f2cd
Leandro Lopes <leandrolopeslive21@hotmail.com>
2025-11-06 19:35:11 -0300

    Atualização

--- inputs/aplicacao.js
+++ inputs/aplicacao.js
@@ -1,19 +1,66 @@
 import escolha from 'readline-sync';
 
-let dados = [
-    { ano: 2010, salarioMinimo: 510.00, ipca: 5.91 },
-    { ano: 2011, salarioMinimo: 545.00, ipca: 6.50 },
-    { ano: 2012, salarioMinimo: 622.00, ipca: 5.84 },
-    { ano: 2013, salarioMinimo: 678.00, ipca: 5.91 },
-    { ano: 2014, salarioMinimo: 724.00, ipca: 6.41 },
-    { ano: 2015, salarioMinimo: 788.00, ipca: 10.67 },
-    { ano: 2016, salarioMinimo: 880.00, ipca: 6.29 },
-    { ano: 2017, salarioMinimo: 937.00, ipca: 2.95 },
-    { ano: 2018, salarioMinimo: 954.00, ipca: 3.75 },
-    { ano: 2019, salarioMinimo: 998.00, ipca: 4.31 },
-    { ano: 2020, salarioMinimo: 1045.00, ipca: 4.52 },
+let salarioMinimo = [
+    { ano: 2010, salario: 510 },
+    { ano: 2011, salario: 545 },
+    { ano: 2012, salario: 622 },
+    { ano: 2013, salario: 678 },
+    { ano: 2014, salario: 724 },
+    { ano: 2015, salario: 788 },
+    { ano: 2016, salario: 880 },
+    { ano: 2017, salario: 937 },
+    { ano: 2018, salario: 954 },
+    { ano: 2019, salario: 998 },
+    { ano: 2020, salario: 1045 },
 ];
 
+let dadosIpca = [
+    { ano: 2010, ipca: 5.91 },
+    { ano: 2011, ipca: 6.50 },
+    { ano: 2012, ipca: 5.84 },
+    { ano: 2013, ipca: 5.91 },
+    { ano: 2014, ipca: 6.41 },
+    { ano: 2015, ipca: 10.67 },
+    { ano: 2016, ipca: 6.29 },
+    { ano: 2017, ipca: 2.95 },
+    { ano: 2018, ipca: 3.75 },
+    { ano: 2019, ipca: 4.31 },
+    { ano: 2020, ipca: 4.52 },
+];
+
+console.log("Escolha uma das alternativas:\n");
+console.log("1 - Listar os salários mínimos de 2010 à 2020");
+console.log("2 - Listar o índice IPCA de 2010 à 2020");
+console.log("3 - Comparação entre o percentual de aumento salarial e o IPCA\n");
+
+let numEscolhido = escolha.question("Digite o número da sua escolha: ");
+
+for (let i = 0; i < salarioMinimo.length; i++) {
+
+    if (numEscolhido == 1) {
+
+        let ano = salarioMinimo[i].ano;
+        let salario = salarioMinimo[i].salario;
+        console.log("\nAno: ".padEnd(30, ".") + ano);
+        console.log("Salário Mínimo: ".padEnd(30, ".") + "R$ " + salario + ",00");
+
+    } else if (numEscolhido == 2) {
+
+        let ano = dadosIpca[i].ano;
+        let ipca = dadosIpca[i].ipca;
+        console.log("\nAno: ".padEnd(30, ".") + ano);
+        console.log("Índice IPCA: ".padEnd(30, ".") + ipca.toFixed(2) + "%");
+        
+    } else {
+
+        console.log("\nOpção inválida. Por favor, escolha 1, 2 ou 3.\n");
+        break;
+    }
+}
+
+// console.log("\nVocê escolheu a opção " + numEscolhido + ".\n");
+
+/*
 for (let dado of dados) {
 
     let ano =+ dado.ano;
@@ -27,12 +74,7 @@ for (let dado of dados) {
     // console.log("Índice IPCA: ".padEnd(30, ".") + ipca.toFixed(2) + "%");
 
     
-    console.log("Escolha uma das alternativas:\n");
-    console.log("1 - Listar os salários mínimos de 2010 à 2020");
-    console.log("2 - Listar o índice IPCA de 2010 à 2020");
-    console.log("3 - Comparação entre o percentual de aumento salarial e o IPCA\n");
-
-    let numEscolhido = escolha.question("Digite o número da sua escolha: ");
+    
 
     if (numEscolhido == 1) {
 
@@ -70,4 +112,5 @@ for (let dado of dados) {
     }
 
 }
+*/
 
---
+++ inputs/colecao_dados_salario.js
@@ -0,0 +1,13 @@
+let salarioMinimo = [
+    { ano: 2010, salario: 510 },
+    { ano: 2011, salario: 545 },
+    { ano: 2012, salario: 622 },
+    { ano: 2013, salario: 678 },
+    { ano: 2014, salario: 724 },
+    { ano: 2015, salario: 788 },
+    { ano: 2016, salario: 880 },
+    { ano: 2017, salario: 937 },
+    { ano: 2018, salario: 954 },
+    { ano: 2019, salario: 998 },
+    { ano: 2020, salario: 1045 },
+];
