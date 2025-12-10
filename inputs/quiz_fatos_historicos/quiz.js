9caa7d5403fd8888c984661fa1605fc75b91ef2d
Leandro Lopes <leandrolopeslive21@hotmail.com>
2025-12-09 21:33:44 -0300

    Novo quiz

---
+++ inputs/quiz_fatos_historicos/quiz.js
@@ -0,0 +1,106 @@
+import dados from 'readline-sync';
+
+const questoes = [
+  { id: 1, pergunta: 'Quando aconteceu o atentado as Torres Gêmeas?', resposta: '2001' },
+  { id: 2, pergunta: 'Em que ano foi detectado o primeiro paciente com coronavírus?', resposta: '2019' },
+  { id: 3, pergunta: 'Em que ano Steve Jobs apresentou o primeiro iPhone sem botão físico?', resposta: '2007' },
+  { id: 4, pergunta: 'Qual o ano da morte de Stan Lee?', resposta: '2018' },
+  { id: 5, pergunta: 'Qual ano o Brasil tornou-se tetracampeão mundial de futebol?', resposta: '1994' },
+  { id: 6, pergunta: 'O primeiro lançamento de foguete da SpaceX ocorreu em?', resposta: '2017' },
+  { id: 7, pergunta: 'Em que ano foi apresentado o primeiro computador eletrônico?', resposta: '1946' },
+  { id: 8, pergunta: 'Em que ano ocorreu a Revolução Francesa?', resposta: '1789' },
+  { id: 9, pergunta: 'Em que ano começou a Primeira Guerra Mundial?', resposta: '1914' },
+  { id: 10, pergunta: 'Em que ano terminou a Segunda Guerra Mundial?', resposta: '1945' },
+  { id: 11, pergunta: 'O muro de Berlim foi derrubado em qual ano?', resposta: '1989' },
+  { id: 12, pergunta: 'Qual o ano que o Titanic afundou?', resposta: '1912'  },
+  { id: 13, pergunta: 'Qual o ano de lançamento do primeiro PlayStation?', resposta: '1994' },
+  { id: 14, pergunta: 'Em que ano a Declaração Universal dos Direitos Humanos foi assinada?', resposta: '1948' },
+  { id: 15, pergunta: 'Em que ano a World Wide Web (WWW) foi criada por Tim Berners-Lee?', resposta: '1990' },
+  { id: 16, pergunta: 'Em que ano o Euro foi introduzido como moeda única na Europa?', resposta: '1999' },
+  { id: 17, pergunta: 'Em que ano foi abolido a escravidão no Brasil?', resposta: '1888' },
+  { id: 18, pergunta: 'Qual ano foi instaurado o regime militar no Brasil?', resposta: '1964' },
+  { id: 19, pergunta: 'Em que ano o Brasil proclamou a República?', resposta: '1889' },
+  { id: 20, pergunta: 'Em qual ano ocorreu a Revolta da Vacina no Rio de Janeiro?', resposta: '1904' },
+  { id: 21, pergunta: 'Em que ano o Brasil sediou a Copa do Mundo pela primeira vez?', resposta: '1950' },
+  { id: 22, pergunta: 'Em que ano foi adotado o Acordo de Paris sobre mudanças climáticas?', resposta: '2015' },
+  { id: 23, pergunta: 'Em qual ano ocorreu a guerra das Malvinas?', resposta: '1982' },
+  { id: 24, pergunta: 'Qual foi o ano em que Martin Luther King realizou o discurso "Eu Tenho um Sonho"?', resposta: '1963' },
+  { id: 25, pergunta: 'Em que ano a União Europeia (UE) foi estabelecida?', resposta: '1993' },
+  { id: 26, pergunta: 'Em que ano a Guerra Civil Espanhola começou?', resposta: '1936' },
+  { id: 27, pergunta: 'Em que ano Nelson Mandela foi libertado da prisão?', resposta: '1990' },
+  { id: 28, pergunta: 'Em que ano a Alemanha foi reunificada após a Guerra Fria?', resposta: '1990' },
+  { id: 29, pergunta: 'Qual foi o ano do acidente nuclear em Chernobyl?', resposta: '1986' },
+  { id: 30, pergunta: 'Em que ano o homem pisou na Lua pela primeira vez?', resposta: '1969' }
+];
+
+function selecionarQuestoesAleatorias(questoes, quantidadeQuestoes) {
+    const questoesAleatorias = questoes.sort(() => Math.random() - 0.5);
+    return questoesAleatorias.slice(0, quantidadeQuestoes);
+}
+
+function exibirPergunta(pergunta, index) {
+    console.log(`\n${index + 1}: ${pergunta.pergunta}`);
+    const respostaUsuario = dados.question(`Digite a resposta: `);
+    return respostaUsuario;
+}
+
+function validarRespostaDoUsuario(respostaDoUsuario, pergunta) {
+    return respostaDoUsuario === pergunta.resposta;
+}
+
+function exibirResultado(pontuacaoFinal, nomeJogador) {
+
+    let mensagem = '';
+
+    if(pontuacaoFinal <= 3) {
+        mensagem = 'OH NÃO! Tente mais uma vez.';
+    } else if(pontuacaoFinal <= 6) {
+        mensagem = 'BOM TRABALHO! Pratique um pouco mais.';
+    } else if(pontuacaoFinal <= 9) {
+        mensagem = 'MUITO BOM! Você acertou a maioria.';
+    } else {
+        mensagem = 'EXCELENTE! Você é um verdadeiro expert.';
+    }
+
+    console.log(`\nNome do jogador: ${nomeJogador}`);
+    console.log(`Pontuação: ${pontuacaoFinal} pontos`);
+    console.log(`${mensagem}`);
+}
+
+function iniciarQuiz() {
+
+    console.log(`\n***** QUIZ DE FATOS HISTÓRICOS *****`);
+    const nomeJogador = dados.question(`Digite seu nome: `);
+
+    const quantidadeQuestoes = 10;
+
+    const questoesSelecionadas = selecionarQuestoesAleatorias(questoes, quantidadeQuestoes);
+
+    let pontuacaoFinal = 0;
+
+    questoesSelecionadas.forEach((pergunta, index) => {
+        const respostaUsuario = exibirPergunta(pergunta, index);
+        const resultadoValidacao = validarRespostaDoUsuario(respostaUsuario, pergunta);
+
+        if(resultadoValidacao) {
+            console.log(`Resposta correta!`);
+            pontuacaoFinal++;
+        } else {
+            console.log(`Resposta incorreta!`);
+        }
+
+        exibirResultado(pontuacaoFinal, nomeJogador);
+    });
+}
+
+iniciarQuiz();
+
+/* Mensagem de encerramento */
+
+/*
+pontuação final ------ mensagem de encerramento
+1 a 3 ---------------- OH NÃO! Tente mais uma vez.
+4 a 6 ---------------- BOM TRABALHO! Pratique um pouco mais.
+7 a 9 ---------------- MUITO BOM! Você acertou a maioria.
+10 ------------------- EXCELENTE! Você é um verdadeiro expert.
+*/
