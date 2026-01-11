b6160c131841baa0bf494763a82990a90df548cf
Leandro Lopes <leandrolopeslive21@hotmail.com>
2026-01-11 13:32:05 -0300

    Estudos com React

---
+++ react/minha-pagina-componentes/src/componentes/Rodape/index.jsx
@@ -0,0 +1,9 @@
+import estilos from "./Rodape.module.css";
+
+export default function Rodape() {
+    return (
+        <footer className={estilos.rodape}>
+          <h2>Desenvolvido com React</h2>
+        </footer>
+    );
+}
