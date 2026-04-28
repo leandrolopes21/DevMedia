f4a96e9dbb5a742042b6cd35969e6af3cc315884
Leandro Lopes <leandrolopeslive21@hotmail.com>
2026-04-28 19:00:20 -0300

    Update index

--- node+express/curioso/index.js
+++ node+express/curioso/index.js
@@ -6,6 +6,9 @@ const curiosidades = [
    {id: 2, titulo: 'Curiosidade 2', descricao: 'O DNA humano, se esticado, alcançaria uma distância de cerca de 16 bilhões de quilômetros.'},
    {id: 3, titulo: 'Curiosidade 3', descricao: 'As baleias-azuis são os maiores animais que já existiram na Terra.'},
    {id: 4, titulo: 'Curiosidade 4', descricao: 'O Sol leva cerca de 8 minutos e 20 segundos para a luz percorrer a distância até chegar à Terra.'},
+   {id: 5, titulo: 'Curiosidade 5', descricao: 'O coração de um beija-flor pode bater mais de 1.200 vezes por minuto.'},
+   {id: 6, titulo: 'Curiosidade 6', descricao: 'Vênus é o único planeta do sistema solar que gira no sentido horário.'},
+   {id: 7, titulo: 'Curiosidade 7', descricao: 'Existem mais estrelas no universo do que grãos de areia em todas as praias da Terra.'},
 ];
 
 function retornaCuriosidade() {
