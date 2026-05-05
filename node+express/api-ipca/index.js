23cd40171e5a158657032f4da146c00129ee88cf
Leandro Lopes <leandrolopeslive21@hotmail.com>
2026-05-05 12:31:59 -0300

    Atualizando

--- node+express/api-ipca/index.js
+++ node+express/api-ipca/index.js
@@ -2,14 +2,15 @@ import { exibeDadosIpca, exibeDadosIpcaPorAno, exibeDadosPorID } from './servico
 import express from 'express';
 const app = express();
 
+// 1ª Rota, mostra todos os dados entre 01/2015 e 12/2023, ou filtra por ano específico
 app.get('/inflacao', (req, res) => {
     const dadosIpca = req.query.dados;
     const dadosIpcaAno = req.query.busca;
-
     const retornaDados = dadosIpcaAno ? exibeDadosIpcaPorAno(dadosIpcaAno) : exibeDadosIpca();
     res.json(retornaDados);
 });
 
+// 2ª Rota, busca os dados através do seu id
 app.get('/inflacao/:id', (req, res) => {
     const dadosIpcaID = req.params.id;
     const retornaDadosIpcaID = exibeDadosPorID(dadosIpcaID);
