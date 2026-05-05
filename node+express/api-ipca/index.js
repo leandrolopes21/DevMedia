3854a1089d5a37abee376cec3bbf9a0bbac28ffd
Leandro Lopes <leandrolopeslive21@hotmail.com>
2026-05-05 18:57:23 -0300

    Update index.js

--- node+express/api-ipca/index.js
+++ node+express/api-ipca/index.js
@@ -5,9 +5,16 @@ const app = express();
 // 1ª Rota, mostra todos os dados entre 01/2015 e 12/2023, ou filtra por ano específico
 app.get('/inflacao', (req, res) => {
     const dadosIpca = req.query.dados;
-    const dadosIpcaAno = req.query.busca;
-    const retornaDados = dadosIpcaAno ? exibeDadosIpcaPorAno(dadosIpcaAno) : exibeDadosIpca();
-    res.json(retornaDados);
+    const dadosIpcaAno = req.query.ano;
+    // const retornaDados = dadosIpcaAno ? exibeDadosIpcaPorAno(dadosIpcaAno) : exibeDadosIpca();
+
+    if (isNaN(dadosIpcaAno)) {
+        res.status(400).json({"Erro": "Requisição inválida"});
+    } else if (dadosIpcaAno < 2015 && dadosIpcaAno > 2023) {
+        res.status(404).json({"Erro": "Parâmetro de busca não encontrado"});
+    } else {
+        if () {};
+    }
 });
 
 // 2ª Rota, busca os dados através do seu id
