315e9a10d2e2c86fe430246c936fe6ee74dc05c3
Leandro Lopes <leandrolopeslive21@hotmail.com>
2026-05-05 20:33:57 -0300

    Atualizando index.js

--- node+express/api-ipca/index.js
+++ node+express/api-ipca/index.js
@@ -2,26 +2,46 @@ import { exibeDadosIpca, exibeDadosIpcaPorAno, exibeDadosPorID } from './servico
 import express from 'express';
 const app = express();
 
-// 1ª Rota, mostra todos os dados entre 01/2015 e 12/2023, ou filtra por ano específico
-app.get('/inflacao', (req, res) => {
+// 1ª Rota, mostra todos os dados entre 01/2015 e 12/2023
+// 2ª Rota, filtra por ano específico
+app.get('/historicoIPCA', (req, res) => {
     const dadosIpca = req.query.dados;
     const dadosIpcaAno = req.query.ano;
-    // const retornaDados = dadosIpcaAno ? exibeDadosIpcaPorAno(dadosIpcaAno) : exibeDadosIpca();
 
-    if (isNaN(dadosIpcaAno)) {
-        res.status(400).json({"Erro": "Requisição inválida"});
-    } else if (dadosIpcaAno < 2015 && dadosIpcaAno > 2023) {
-        res.status(404).json({"Erro": "Parâmetro de busca não encontrado"});
+    if (dadosIpcaAno) {
+        const ano = parseInt(dadosIpcaAno);
+        if (isNaN(ano)) {
+            res.status(400).send({"erro": "Requisição inválida!"});
+        } else if (ano < 2015 || ano > 2023) {
+            res.status(404).send({"erro": "Ano não encontrado"});
+        } else {
+            const retornaDadosIpcaPorAno = exibeDadosIpcaPorAno(ano);
+
+            if (retornaDadosIpcaPorAno.length > 0) {
+                res.json(retornaDadosIpcaPorAno);
+            }
+        }
     } else {
-        if () {};
+        const retornaDados = exibeDadosIpca();
+        res.json(retornaDados);
     }
 });
 
-// 2ª Rota, busca os dados através do seu id
-app.get('/inflacao/:id', (req, res) => {
+// 3ª Rota, busca os dados através do seu id
+app.get('/historicoIPCA/:id', (req, res) => {
     const dadosIpcaID = req.params.id;
-    const retornaDadosIpcaID = exibeDadosPorID(dadosIpcaID);
-    res.json(retornaDadosIpcaID);
+
+    if (isNaN(parseInt(dadosIpcaID))) {
+        res.status(400).send({"erro": "Requisição inválida!"});
+    } else {
+        const retornaDadosIpcaID = exibeDadosPorID(dadosIpcaID);
+
+        if (retornaDadosIpcaID) {
+            res.json(retornaDadosIpcaID);
+        } else {
+            res.status(404).send({"erro": "ID não encontrada"});
+        }
+    }
 });
 
 app.listen(8080, () => {
