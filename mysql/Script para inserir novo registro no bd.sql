b3cbe64e3a848c1bf89fb3ded827f57349d68769
Leandro Lopes <leandrolopeslive21@hotmail.com>
2026-05-24 12:09:33 -0300

    Atualização local

--- mysql/Script para inserir novo registro no bd.sql
+++
@@ -1,6 +0,0 @@
--- Inserir novo registro na tabela funcionarios
-
-/*
-INSERT INTO funcionarios (nome, cpf, data_admissao, id_departamento, id_cargo)
-values ('Sonia Maria Lopes', '05265334814', '1999-10-14', 1, 2);
-*/
