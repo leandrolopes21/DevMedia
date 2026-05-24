f2e08bd2acea6c3dc390f2e2bafa1e085a3bcdfb
Leandro Lopes <leandrolopeslive21@hotmail.com>
2026-05-24 14:44:20 -0300

    Atualizado script de usuário remoto

--- mysql/Criar usuario para acesso remoto.sql
+++ mysql/Criar usuario para acesso remoto.sql
@@ -1,8 +1,8 @@
 -- Criar usuário para acesso remoto
-CREATE USER 'leandro@%' IDENTIFIED BY '*Leandro21';
+CREATE USER 'leandro'@'%' IDENTIFIED BY '*Leandro21';
 
 -- Todas as permissões para gerenciar os bancos
-GRANT ALL PRIVILEGES ON *.* TO 'leandro@%';
+GRANT ALL PRIVILEGES ON *.* TO 'leandro'@'%';
 
 -- Atualiza as permissões do MySQL
 FLUSH PRIVILEGES;
