65e856735dc385d0702c5f1fda8173609bae6ea4
Leandro Lopes <leandrolopeslive21@hotmail.com>
2026-05-28 15:26:17 -0300

    Atualizando arquivos sql

---
+++ mysql/Alterar atributo de coluna.sql
@@ -0,0 +1,4 @@
+-- Alterar atributo de coluna
+
+ALTER TABLE curitibaflor.clientes
+MODIFY COLUMN telefone VARCHAR(9) NOT NULL;
---
+++ mysql/Inserindo dados na tabela clientes do BD curitibaflor.sql
@@ -0,0 +1,17 @@
+-- Inserindo dados na tabela clientes do banco de dados curitibaflor
+
+INSERT INTO curitibaflor.clientes (
+    nome, email, ddd_telefone, telefone, data_cadastro
+) VALUES
+    ('Leandro', 'leandro@email.com', '041', '998875412', '2026-05-28'),
+    ('Lucas', 'lucas@email.com', '041', '978875685', '2026-05-28'),
+    ('Marize', 'marize@email.com', '041', '998745962', '2026-05-28'),
+    ('Ricardo', 'ricardo@email.com', '031', '988552233', '2026-05-28'),
+    ('Beatriz', 'beatriz@email.com', '044', '995544332', '2026-05-28'),
+    ('Felipe', 'felipe@email.com', '088', '991122334', '2026-05-28'),
+    ('Camila', 'camila@email.com', '019', '987654321', '2026-05-28'),
+    ('Gustavo', 'gustavo@email.com', '042', '992233445', '2026-05-28'),
+    ('Isabela', 'isabela@email.com', '047', '988776655', '2026-05-28'),
+    ('Daniel', 'daniel@email.com', '011', '993344556', '2026-05-28'),
+    ('Larissa', 'larissa@email.com', '011', '987788990', '2026-05-28'),
+    ('Rafael', 'rafael@email.com', '019', '994455667', '2026-05-28');
